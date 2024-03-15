389a9b13ffd9bdbe9d7173aec4d98f2d0794ccc3
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2024-03-15 13:12:03 +0900

    [level 3] Title: 야근 지수, Time: 102.01 ms, Memory: 67.6 MB -BaekjoonHub

--- Java/프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ Java/프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,60 +1,26 @@
+import java.util.*;
+
 class Solution {
     public long solution(int n, int[] works) {
-        int total = 0;
-        for (int i : works) {
-            total += i;
-        }
-        if (total <= n)
-            return 0;
+        PriorityQueue<Integer> pq = new PriorityQueue(Comparator.reverseOrder());
         
-        int low = 0, high = 1000000, mid;
-        int averageWork = 0;
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (workPossible(mid, works, n) == 1) {
-                averageWork = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        
-        System.out.println(averageWork);
-        
-        for (int i = 0; i < works.length; i++) {
-            if (works[i] > averageWork) {
-                n -= works[i] - averageWork;
-                works[i] = averageWork;
-            }
-        }
-        
-        for (int i = 0; i < works.length; i++) {
-            if (n == 0)
+        for (int i : works) pq.offer(i);
+        int maxWork = 0;
+        while (n > 0) {
+            maxWork = pq.poll();
+            if (maxWork == 0) {
+                pq.offer(maxWork);
                 break;
-            if (works[i] == averageWork) {
-                works[i]--;
-                n--;
             }
+            maxWork--;
+            pq.offer(maxWork);
+            n--;
         }
-        
         long answer = 0;
-        for (int i : works) {
+        for (int i : pq) {
+            // System.out.print(i + " ");
             answer += i * i;
         }
         return answer;
-    }
-    
-    private int workPossible(int mid, int[] works, int n) {
-        int workTime = 0;
-        
-        for (int w : works) {
-            if (w > mid) {
-                workTime += w - mid;
-            }
-        }
-        
-        if (workTime > n)
-            return 0;
-        return 1;
-    }
+    }    
 }
