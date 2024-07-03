92f9ec1b3071aece3bd4d7d8d7d39df5064d40df
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2024-07-03 17:39:22 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 50.68 ms, Memory: 217 MB -BaekjoonHub

--- Java/프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ Java/프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -4,57 +4,24 @@ class Solution {
         
         for (int[] s : skill) {
             if (s[0] == 1)
-                s[5] *= -1;
+	            s[5] *= -1;
             sum[s[1]][s[2]] += s[5];
             sum[s[1]][s[4] + 1] += s[5] * -1;
             sum[s[3] + 1][s[2]] += s[5] * -1;
             sum[s[3] + 1][s[4] + 1] += s[5];
-            for (int i = 0; i < sum.length; i++) {
-                for (int j = 0; j < sum[0].length; j++) {
-                    if (sum[i][j] >= 0)
-                        System.out.print(" ");    
-                    System.out.print(sum[i][j] + " ");
-                }
-                System.out.println("");
-            }
-            System.out.println("-------------------------");
         }
         
-                for (int j = 0; j < sum[0].length; j++) { // 세로 방향 부분합 계산
-            int prefix = sum[0][j];
-            for (int i = 1; i < sum.length; i++) {
-                sum[i][j] += prefix;
-                prefix = sum[i][j];
-            }
+				for (int j = 0; j < sum[0].length; j++) { // 세로 방향 부분합 계산
+            for (int i = 1; i < sum.length; i++)
+                sum[i][j] += sum[i - 1][j];
         }
-            for (int i = 0; i < sum.length; i++) {
-                for (int j = 0; j < sum[0].length; j++) {
-                    if (sum[i][j] > 0)
-                        System.out.print(" ");    
-                    System.out.print(sum[i][j] + " ");
-                }
-                System.out.println("");
-            }
-        System.out.println("-------------------------");
-        int answer = 0;
         
         for (int i = 0; i < sum.length; i++) { // 가로 방향 부분합 계산
-            int prefix = sum[i][0];
-            for (int j = 1; j < sum[0].length; j++) {
-                sum[i][j] += prefix;
-                prefix = sum[i][j];
-            }
+            for (int j = 1; j < sum[0].length; j++)
+                sum[i][j] += sum[i][j - 1];
         }
-            for (int i = 0; i < sum.length; i++) {
-                for (int j = 0; j < sum[0].length; j++) {
-                    if (sum[i][j] > 0)
-                        System.out.print(" ");    
-                    System.out.print(sum[i][j] + " ");
-                }
-                System.out.println("");
-            }
-        System.out.println("-------------------------");
-
+        
+        int answer = 0;
         
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
